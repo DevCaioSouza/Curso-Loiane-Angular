6bb6ec2f124e97f9eb05923f0a983eacd8781540
unknown <caiocbahia@hotmail.com>
2023-03-03 23:37:17 -0300

    botão com evento click boolean + utilizando hidden

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent {
+
+  cursos: string[] = ["Angular"];
+
+  mostrarCursos: boolean = false;
+
+  onExibirCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
+}
