08d9afb8a8fe7de1090a772dc5a1b15dc6f34726
unknown <caiocbahia@hotmail.com>
2023-03-05 16:09:36 -0300

    #29 ngClass finalizado

--- diretivas/src/app/diretiva-ng-class/diretiva-ng-class.component.ts
+++ diretivas/src/app/diretiva-ng-class/diretiva-ng-class.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class DiretivaNgClassComponent {
 
+  meuFavorito: boolean = false;
+
+  onClickOnStar(){
+    this.meuFavorito = !this.meuFavorito
+  }
+
 }
